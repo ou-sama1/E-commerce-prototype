42d2d727316d9769eba84679937052937c8a9f57
Oussama <xdooussama@gmail.com>
2024-02-14 21:56:22 +0100

    Adding client side pagination

---
+++ frontend/src/components/UI/Pagination.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import styles from './Pagination.module.css';
+
+const Pagination = (props) => {
+    const { itemsCount, itemsPerPage, currentPage, MoveToNextPage } = props;
+    
+    //Calculating the number of occupied pages
+    const numberOfPages = Math.ceil(itemsCount / itemsPerPage);
+
+    const[pages, setPages] = useState([])
+
+    //Setting the max number of visible pages you can navigate to
+    const maxVisiblePages = 3;
+
+    useEffect(()=>{
+        //An array that start from 1 to the number of occupied pages
+        setPages(Array((numberOfPages <= maxVisiblePages) ? numberOfPages : maxVisiblePages).fill().map((_, i) => i+1))
+        console.log("useEffect triggered")
+    },[numberOfPages])
+    
+    const handlePagination = (page) => {
+        //Changing the pages state based on which page is clicked, if its the last but not the end of pages, the pages array will update to push another page and drop the first one and vice versa
+        if(page === pages[pages.length - 1] && page < numberOfPages) {
+            setPages(prev => [...prev.filter((_, i) => i !== 0), page + 1])
+        }
+        else if(page === pages[0] && page > 1){
+            setPages(prev => [page - 1, ...prev.filter((_, i) => i !== pages.length - 1)])
+        }
+        MoveToNextPage(page)
+    }
+
+    return(
+        <ul className={styles.container}>
+            {
+                pages.map( i => 
+                                <li className={
+                                        `${styles.num} 
+                                        ${currentPage === i ? styles.active : ''}`
+                                        } 
+                                    key={i} 
+                                    onClick={()=>handlePagination(i)
+                                }>
+                                    {/* Rendering arrows depending on the condition mentioned in the previous comment */}
+                                    {i === pages[0] && i > 1 && '<<'}
+                                    {i === pages[pages.length - 1] && i < numberOfPages && '>>'}
+                                    {!(i === pages[0] && i > 1) && !(i === pages[pages.length - 1] && i < numberOfPages) && i}
+                                </li>
+                                )
+            }
+        </ul>
+    )
+}
+
+export default Pagination;
--- frontend/src/components/products/ProductsList.jsx
+++ frontend/src/components/products/ProductsList.jsx
@@ -5,38 +5,64 @@ import Filter from "../filter/Filter.jsx";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import ProductSkeleton from "./skeleton/ProductSkeleton.jsx";
+import Pagination from "../UI/Pagination.jsx";
 
 const ProductsList = (props) => {
+    const [currentPage, setCurrentPage] = useState(1);
     const {category, gender, price} = useSelector(state => state.filter);
     let {products, loading, error} = useProducts();
 
-    return(
-        <div className={styles.container}>
-            <Filter />
-            <ul className={styles.productsList}>
+    const productsPerPage = 8;
 
-                {
-                    (loading && !products.length && !error) && Array(25).fill(0).map((_ , i)=> <ProductSkeleton key={i} />)
-                }
-                {
-                    (error && !products.length) && "An error occured."
-                }
-                {
-                    products && products
-                        .filter(
-                            product => (
-                                (product.category === (category || product.category))
-                                &&
-                                (product.gender === (gender || product.gender))
-                                &&
-                                ( price ? (product.price >= parseFloat(price.split("-")[0]) && product.price <= parseFloat(price.split("-")[1])) : true)
+    const MoveToNextPage = (nextPage) => {
+        setCurrentPage(prev => nextPage)
+    }
+
+    //Reset the current page to 1 when filtered by either category, gender, price
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [category, gender, price])
+
+    //Filter by category, gender, price
+    const filteredProducts = products
+                            .filter(
+                                (product) => (
+                                    (product.category === (category || product.category))
+                                    &&
+                                    (product.gender === (gender || product.gender))
+                                    &&
+                                    ( price ? (product.price >= parseFloat(price.split("-")[0]) && product.price <= parseFloat(price.split("-")[1])) : true)                                
+                                )
                             )
-                        )
-                        .map(product => <Product key={product.id} product={product} />)
-                }
+                            .map(product => <Product key={product.id} product={product} />)
 
-            </ul>
-        </div>
+    return(
+        <>
+            <div className={styles.container}>
+                <Filter />
+                <ul className={styles.productsList}>
+
+                    {
+                        (loading && !products.length && !error) && Array(productsPerPage).fill(0).map((_ , i)=> <ProductSkeleton key={i} />)
+                    }
+                    {
+                        (error && !products.length) && "An error occured."
+                    }
+                    {
+                        // Filter by page (pagination)
+                        products && filteredProducts
+                                    .filter(
+                                            (_, i) => (i < (currentPage * productsPerPage) && i >= (currentPage * productsPerPage - productsPerPage))
+                                        )
+                    }
+
+                </ul>
+                
+            </div>
+            {
+                products && <Pagination itemsCount={filteredProducts.length} itemsPerPage={productsPerPage} MoveToNextPage={MoveToNextPage} currentPage={currentPage} />
+            }
+        </>
     )
 }
 
